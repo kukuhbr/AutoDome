f0b4beb65058b57a1da5cd0d83094ff9642f555a
kukuhbr <kukuhis777@gmail.com>
2020-12-15 22:14:08 +0700

    add vehicle upgrade database

---
+++ Assets/Scripts/DatabaseVehicleUpgrade.cs
@@ -0,0 +1,14 @@
+﻿
+using UnityEngine;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObject/Database/DatabaseVehicleUpgrade", order = 1)]
+public class DatabaseVehicleUpgrade : ScriptableObject
+{
+    [SerializeField]
+    private List<VehicleUpgrade> vehicleUpgradeDB;
+
+    public VehicleUpgradeData GetUpgradeRequirement(int id, int grade) {
+        return vehicleUpgradeDB[id].grade[grade];
+    }
+}
---
+++ Assets/Scripts/Player/ScriptableObjects/VehicleUpgrade.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObject/Vehicle/VehicleUpgrade", order = 1)]
+public class VehicleUpgrade : ScriptableObject
+{
+    public int id;
+    [SerializeField]
+    public List<VehicleUpgradeData> grade;
+}
+
+[Serializable]
+public class VehicleUpgradeData {
+    public List<ItemRequirement> requirements;
+}
+
+[Serializable]
+public class ItemRequirement {
+    public ItemBase item;
+    public int quantity;
+}
+
+
