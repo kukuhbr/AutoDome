9f737210dd5c12423017fb9a534dd01fea2f4be7
kukuhbr <kukuhis777@gmail.com>
2020-12-27 16:47:33 +0700

    add google admob package

---
+++ Assets/InstantNotification.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantNotification : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DestroyAfter(.8f));
+    }
+
+    IEnumerator DestroyAfter(float time) {
+        yield return new WaitForSeconds(time);
+        Destroy(gameObject);
+    }
+}
